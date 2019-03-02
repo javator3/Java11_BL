c7089c802d6a286d87de2a07ed0e572e04051da9
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-03-02 14:50:33 +0100

    examples

--- src/pl/java/Problems/Main.java
+++ src/pl/java/Problems/Main.java
@@ -1,8 +1,25 @@
 package pl.java.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+     var zmienna = "Wartosc";
+     var list = new ArrayList<String>();
+     list.add("test");
+
+        System.out.println(zmienna);
+        System.out.println(list);
+
+        var l = List.of("A", "B", "C");
+        var copy = List.copyOf(l);
+
+        var map = Map.of("A", 1, "B", 2);
+        System.out.println(map);
+
     }
 }
